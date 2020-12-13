3d3bc861065c414343ad906bd2d34a8b3bcb6a7c
ZacharyACoon <69929375+ZacharyACoon@users.noreply.github.com>
2020-12-13 03:59:24 -0800

    .

---
+++ reference.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+trezorctl get-address --script-type=address --address "m/44'/0'/0'/0/0" #--show-display # 1
+trezorctl get-address --script-type=p2shsegwit --address "m/49'/0'/0'/0/0" #--show-display # 3
+trezorctl get-address --script-type=segwit --address "m/84'/0'/0'/0/0" #--show-display # bc1
